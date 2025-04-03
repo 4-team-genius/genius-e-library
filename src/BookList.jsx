37cb743ba416a7b5712d48292f5bb6ed24a7922d
Alvan Crittenden <arashodc@live.com>
2025-04-02 20:09:55 -0400

    fixed intentation and fixed missing return

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -3,14 +3,33 @@ import {useEffect, useState} from 'react';
 const BookList = (props) => {
   const [books, setBooks] = useState([]);
 
-useEffect(() => {
-  const getBooks = async () => {
-    const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books')
-    const responseJson = await response.json()
-    setBooks(responseJson.books)
-  }
+  useEffect(() => {
+    const getBooks = async () => {
+      const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books')
+      const responseJson = await response.json()
+      setBooks(responseJson.books)
+    }
   getBooks();
+  
 }, []);
+  const contactClick = (book) => {
+    props.setSelectedBook(book);
+  };
+
+  return (
+    <ul>
+      {books.map((individualBook) => {
+        return (
+          <li
+            onClick={() => {contactClick(individualBook)}}
+            key={individualBook.id}
+            >
+            {individualBook.title}
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
 export default BookList;
